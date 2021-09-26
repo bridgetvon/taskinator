b042b21ae0801c28904a5889bdb43146aada0fd2
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-09-26 14:37:02 -0600

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//using the DOM//
+console.dir(window.document);
+
+//save-task button 
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+//task-item list append and dynamic styling 
+//four steps key to create a DOM
+// 1. creat new task item 2 style new task item 3. add the text 4. append this element to task list 
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task. ";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
